08419be0e1cd97f42d3f616861c9d308c99ac674
David Stoica <dstoica@generalmagic.com>
2023-08-10 14:17:25 +0300

    Added Greg.

--- GameLib/GameUI/main.cpp
+++ GameLib/GameUI/main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setWindowIcon(QIcon("res/greg.png"));
     std::shared_ptr<ChessUIQt> w = std::make_shared<ChessUIQt>();
 
 	auto game = IGame::Produce();
